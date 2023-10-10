b8f24b7d825cc87a7e3f52ead82379ef823c5915
Earl <earlmunyua96@gmail.com>
2023-10-10 06:42:13 +0300

    array.find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+    function superbowlWin(record) {
+    const winRecord = record.find((array) => array.result ==="W"
+    )
+    if (winRecord) {
+       return winRecord.year 
+    }else{
+        return undefined
+    }
+  }
+
